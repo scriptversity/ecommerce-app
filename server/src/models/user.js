c9cb50f0664d7984174aa634efd32155be2bcb8f
Script Versity <scriptversity@yahoo.com>
2025-06-23 13:55:59 -0500

    user model tweaked

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -72,7 +72,7 @@ userSchema.methods.getResetPasswordToken = function () {
   // Hash and set to resetPasswordToken
   this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex')
   // Set token expire time
-  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000 // 30 minutes
   return resetToken
 }
 
