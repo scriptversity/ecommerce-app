48c1400c408f40256e151e501dab7b30a8d0a74d
Casily Coaquira <leocodereng@gmail.com>
2025-06-23 21:54:26 -0500

    feat(usercontroller): updatePassword implemented
    updatePassword implemented and applied to the related code

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/user.js");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
 
 // Get currently logged in user details => GET /api/v1/users/me
 const getUserProfile = catchAsyncErrors(async (req, res, next) => {
@@ -14,6 +15,29 @@ const getUserProfile = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+// Update | change user password => PUT /api/v1/users/updatepassword
+const updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Old password is incorrect", 400));
+  }
+
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password does not match", 400));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+  sendToken(user, 200, res);
+});
+
 module.exports = {
   getUserProfile,
+  updatePassword,
 };
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { getUserProfile } = require("../controllers/userController");
+const {
+  getUserProfile,
+  updatePassword,
+} = require("../controllers/userController");
 const { isAuthenticatedUser } = require("../middlewares/auth");
 
 const router = express.Router();
 
 // Route to get user profile
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
+// Route to update user password
+router.route("/updatepassword").put(isAuthenticatedUser, updatePassword);
 
 module.exports = router;
