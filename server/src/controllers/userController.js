81c85698a950d2f3e421f810999c9a3acd8675e9
Casily Coaquira <leocodereng@gmail.com>
2025-06-23 22:18:04 -0500

    feat(usercontroller): updateProfile implemented
    updateProfile implemented and i will complete it later

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -37,7 +37,33 @@ const updatePassword = catchAsyncErrors(async (req, res, next) => {
   sendToken(user, 200, res);
 });
 
+// Update user profile => PUT /api/v1/users/updateprofile
+const updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  // Update avatar if provided: TODO: Implement avatar upload logic in the future
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
 module.exports = {
   getUserProfile,
   updatePassword,
+  updateProfile,
 };
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   getUserProfile,
   updatePassword,
+  updateProfile,
 } = require("../controllers/userController");
 const { isAuthenticatedUser } = require("../middlewares/auth");
 
@@ -11,5 +12,7 @@ const router = express.Router();
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
 // Route to update user password
 router.route("/updatepassword").put(isAuthenticatedUser, updatePassword);
+// Route to update user profile
+router.route("/updateprofile").put(isAuthenticatedUser, updateProfile);
 
 module.exports = router;
